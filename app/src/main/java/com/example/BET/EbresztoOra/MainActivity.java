a16fc9ffcad54823512c59704c635ddfd2ba10a7
David Bukovszki <bukovszki.david@gmail.com>
2018-12-04 21:21:35 +0100

    Initial commit

---
+++ app/src/main/java/com/example/BET/EbresztoOra/MainActivity.java
@@ -0,0 +1,134 @@
+package com.example.BET.EbresztoOra;
+
+import android.annotation.TargetApi;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    AlarmManager alarmManager;
+    private PendingIntent pending_intent;
+
+    private TimePicker alarmTimePicker;
+    private TextView alarmTextView;
+
+    private AlarmReceiver alarm;
+
+
+    MainActivity inst;
+    Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.context = this;
+
+        //alarm = new AlarmReceiver();
+        alarmTextView = (TextView) findViewById(R.id.alarmText);
+
+        final Intent myIntent = new Intent(this.context, AlarmReceiver.class);
+
+        // Ébresztő kezelő meghívása
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        // Ébresztő óra beállítása a kívánt időre
+        final Calendar calendar = Calendar.getInstance();
+
+        alarmTimePicker = (TimePicker) findViewById(R.id.alarmTimePicker);
+
+
+
+        Button start_alarm= (Button) findViewById(R.id.start_alarm);
+        start_alarm.setOnClickListener(new View.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.M)
+
+            @Override
+            public void onClick(View v) {
+
+                calendar.add(Calendar.SECOND, 3);
+
+
+                final int hour = alarmTimePicker.getCurrentHour();
+                final int minute = alarmTimePicker.getCurrentMinute();;
+
+                Log.e("MyActivity", "In the receiver with " + hour + " and " + minute);
+                setAlarmText("You clicked a " + hour + " and " + minute);
+
+
+                calendar.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getCurrentHour());
+                calendar.set(Calendar.MINUTE, alarmTimePicker.getCurrentMinute());
+
+                myIntent.putExtra("extra", "yes");
+                pending_intent = PendingIntent.getBroadcast(MainActivity.this, 0, myIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pending_intent);
+
+                // Megmutatja mikorra állítottuk be az ébreszőt
+
+
+                setAlarmText("Ébresztés ideje: " + hour + ":" + minute);
+
+            }
+
+        });
+
+        Button stop_alarm= (Button) findViewById(R.id.stop_alarm);
+        stop_alarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                int min = 1;
+                int max = 5;
+
+                Random r = new Random();
+                int random_number = r.nextInt(max - min + 1) + min;
+                Log.e("random number is ", String.valueOf(random_number));
+
+                myIntent.putExtra("extra", "no");
+                sendBroadcast(myIntent);
+
+                alarmManager.cancel(pending_intent);
+                setAlarmText("Ébresztő Off!");
+
+            }
+        });
+
+    }
+
+    public void setAlarmText(String alarmText) {
+        alarmTextView.setText(alarmText);
+    }
+
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        inst = this;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        Log.e("MyActivity", "on Destroy");
+    }
+
+
+}
